7dc55bfbd9b744843044dc5d1f00461958c7b791
Piotr Szwarc <quarcu@gmail.com>
2015-11-18 15:11:33 +0100

    Updated epel-release install.

--- definitions/centos7/base.sh
+++ definitions/centos7/base.sh
@@ -2,13 +2,9 @@
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
-cat > /etc/yum.repos.d/epel.repo << EOM
-[epel]
-name=epel
-baseurl=http://download.fedoraproject.org/pub/epel/beta/7/\$basearch
-enabled=1
-gpgcheck=0
-EOM
+yum install -y epel-release
+yum clean all
+yum makecache
 
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils
 
